f2dcca4f78585385c28a926a945d6baa04c11867
Rasarts <rasart.pro@gmail.com>
2018-04-15 15:38:27 +0300

    Added Product class.

--- lib/components/search/search.dart
+++ lib/components/search/search.dart
@@ -9,73 +9,6 @@ import 'package:angular_router/angular_router.dart';
 import 'package:honeydew/services.dart' show SocketService, EventData;
 import 'package:honeydew/services/routes/paths.dart' as paths;
 
-class SearchParams extends MapBase {
-  Map<String, dynamic> _entityMap = new Map<String, dynamic>();
-
-  String get Language => this['Language'];
-  set Language(String value) => this['Language'] = value;
-
-  String get SearchedName => this['SearchedName'];
-  set SearchedName(String value) => this['SearchedName'] = value;
-
-  int get CurrentPage => this['CurrentPage'];
-  set CurrentPage(int value) => this['CurrentPage'] = value;
-
-  int get CountProductsOnPage => this['CountProductsOnPage'];
-  set CountProductsOnPage(int value) => this['CountProductsOnPage'] = value;
-
-  SearchParams(
-      {String SearchedName, int CurrentPage, int CountProductsOnPage}) {
-    this["SearchedName"] = SearchedName;
-    this["CurrentPage"] = CurrentPage;
-    this["CountProductsOnPage"] = CountProductsOnPage;
-  }
-
-  operator [](Object key) => _entityMap[key];
-
-  operator []=(dynamic key, dynamic value) => _entityMap[key] = value;
-
-  get keys => _entityMap.keys;
-
-  remove(key) => _entityMap.remove(key);
-
-  clear() => _entityMap.clear();
-}
-
-class SearchResponse extends SearchParams {
-  Map<String, dynamic> get entityMap => super._entityMap;
-  set entityMap(Map<String, dynamic> map) => super._entityMap = map;
-
-  SearchResponse(
-      {String SearchedName,
-      int CurrentPage,
-      int CountProductsOnPage,
-      int TotalProductsFound,
-      int TotalProductsOnOnePage}) {
-    super.SearchedName = SearchedName;
-    super.CurrentPage = CurrentPage;
-    super.CountProductsOnPage = CountProductsOnPage;
-
-    this
-      ..TotalProductsFound = TotalProductsFound
-      ..TotalProductsOnOnePage = TotalProductsOnOnePage;
-  }
-
-  SearchResponse.fromMap(Map<String, dynamic> map) {
-    entityMap = map;
-  }
-
-  int get TotalProductsOnOnePage => this['TotalProductsOnOnePage'];
-  set TotalProductsOnOnePage(int value) =>
-      this['TotalProductsOnOnePage'] = value;
-
-  int get TotalProductsFound => this['TotalProductsFound'];
-  set TotalProductsFound(int value) => this['TotalProductsFound'] = value;
-
-  String get SearchedName => this['SearchedName'];
-  set SearchedName(String value) => this['SearchedName'] = value;
-}
-
 @Component(
     selector: 'search', templateUrl: 'search.html', styleUrls: ["search.css"])
 class SearchComponent implements OnActivate {
@@ -138,4 +71,126 @@ class SearchComponent implements OnActivate {
           }));
     }
   }
+}
+
+class Product extends MapBase {
+  Map<String, dynamic> _entityMap = new Map<String, dynamic>();
+
+  String get uid => this['uid'];
+  set uid(String value) => this['uid'] = value;
+
+  String get productName => this['productName'];
+  set productName(String value) => this['productName'] = value;
+
+  String get productIri => this['productIri'];
+  set productIri(String value) => this['productIri'] = value;
+
+  String get previewImageLink => this['previewImageLink'];
+  set previewImageLink(String value) => this['previewImageLink'] = value;
+
+  bool get productIsActive => this._entityMap['productIsActive'];
+  set productIsActive(bool value) => this['productIsActive'] = value;
+
+  Product(
+      {String uid,
+      String productName,
+      String productIri,
+      String previewImageLink,
+      bool productIsActive}) {
+    this["uid"] = uid;
+    this["productName"] = productName;
+    this["productIri"] = productIri;
+    this["previewImageLink"] = previewImageLink;
+    this["productIsActive"] = productIsActive;
+  }
+
+  Product.fromMap(Map map) {
+    this._entityMap = map;
+  }
+
+  operator [](Object key) => _entityMap[key];
+
+  operator []=(dynamic key, dynamic value) => _entityMap[key] = value;
+
+  get keys => _entityMap.keys;
+
+  remove(key) => _entityMap.remove(key);
+
+  clear() => _entityMap.clear();
+}
+
+class SearchParams extends MapBase {
+  Map<String, dynamic> _entityMap = new Map<String, dynamic>();
+
+  String get Language => this['Language'];
+  set Language(String value) => this['Language'] = value;
+
+  String get SearchedName => this['SearchedName'];
+  set SearchedName(String value) => this['SearchedName'] = value;
+
+  int get CurrentPage => this['CurrentPage'];
+  set CurrentPage(int value) => this['CurrentPage'] = value;
+
+  int get CountProductsOnPage => this['CountProductsOnPage'];
+  set CountProductsOnPage(int value) => this['CountProductsOnPage'] = value;
+
+  SearchParams(
+      {String SearchedName, int CurrentPage, int CountProductsOnPage}) {
+    this["SearchedName"] = SearchedName;
+    this["CurrentPage"] = CurrentPage;
+    this["CountProductsOnPage"] = CountProductsOnPage;
+  }
+
+  operator [](Object key) => _entityMap[key];
+
+  operator []=(dynamic key, dynamic value) => _entityMap[key] = value;
+
+  get keys => _entityMap.keys;
+
+  remove(key) => _entityMap.remove(key);
+
+  clear() => _entityMap.clear();
+}
+
+class SearchResponse extends SearchParams {
+  Map<String, dynamic> get entityMap => super._entityMap;
+  set entityMap(Map<String, dynamic> map) => super._entityMap = map;
+
+  SearchResponse(
+      {String SearchedName,
+      int CurrentPage,
+      int CountProductsOnPage,
+      int TotalProductsFound,
+      int TotalProductsOnOnePage}) {
+    super.SearchedName = SearchedName;
+    super.CurrentPage = CurrentPage;
+    super.CountProductsOnPage = CountProductsOnPage;
+
+    this
+      ..TotalProductsFound = TotalProductsFound
+      ..TotalProductsOnOnePage = TotalProductsOnOnePage;
+  }
+
+  SearchResponse.fromMap(Map<String, dynamic> map) {
+    entityMap = map;
+    List<Product> products = new List<Product>();
+    for (Map prod in map["Products"]) {
+      products.add(new Product.fromMap(prod));
+    }
+
+    entityMap["Products"] = products;
+  }
+
+  int get TotalProductsOnOnePage => this['TotalProductsOnOnePage'];
+  set TotalProductsOnOnePage(int value) =>
+      this['TotalProductsOnOnePage'] = value;
+
+  int get TotalProductsFound => this['TotalProductsFound'];
+  set TotalProductsFound(int value) => this['TotalProductsFound'] = value;
+
+  String get SearchedName => this['SearchedName'];
+  set SearchedName(String value) => this['SearchedName'] = value;
+
+  List<Product> get Products => this['Products'];
+  set Products(List<Product> value) => this['Products'] = value;
 }
