252ba97a66dda2d7e5a1fba39fffd1100e7fb1e8
Rasarts <rasart.pro@gmail.com>
2018-06-07 07:47:19 +0300

    Adding mock data for server.

---
+++ bin/server_mock.dart
@@ -0,0 +1,82 @@
+library mock_socket;
+
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:honeydew/entities.dart' show EventData;
+
+void main() {
+  int port = 81;
+
+  HttpServer.bind(InternetAddress.loopbackIPv4, port).then((HttpServer server) {
+    print("Server is running on "
+        "'http://${server.address.address}:$port/'");
+
+    server.transform(new WebSocketTransformer()).listen((WebSocket socket) {
+      socket
+          .map((data) => new EventData.from(json.decode(data)))
+          .listen((EventData eventData) {
+//        {
+//          Message:Need items by name,
+//              Data:{
+//        "SearchedName":"sum",
+//        "CurrentPage":1,
+//        "Tot
+//        alProductsForOnePage":1,
+//        "Language":"ru"
+//        },
+//        APIVersion:1.0   .0
+//        }
+        if (eventData.message == "Need items by name") {
+          EventData event = new EventData("Items by name ready");
+          List<Map> products = <Map>[
+            <String, dynamic>{
+              "uid": "1",
+              "productName": "1",
+              "productIri": "http://",
+              "previewImageLink": "http://",
+              "productIsActive": true,
+              "has_price": <Map>[
+                <String, dynamic>{
+                  "uid": "1",
+                  "priceValue": 1,
+                  "priceIsActive": true,
+                  "belongs_to_company": <Map>[
+                    <String, dynamic>{
+                      "uid": "1",
+                      "companyName": "Test company",
+                      "companyIri": "http://",
+                      "companyIsActive": true,
+                    }
+                  ]
+                },
+                <String, dynamic>{
+                  "uid": "2",
+                  "priceValue": 2,
+                  "priceIsActive": true,
+                  "belongs_to_company": <Map>[
+                    <String, dynamic>{
+                      "uid": "1",
+                      "companyName": "Test company",
+                      "companyIri": "http://",
+                      "companyIsActive": true,
+                    }
+                  ]
+                }
+              ]
+            }
+          ];
+
+          Map data = json.decode(eventData.data);
+
+          data["Products"] = products;
+          data["TotalProductsFound"] = 1;
+
+          event.APIVersion = eventData.APIVersion;
+          event.data = json.encode(data);
+          socket.add(json.encode(event));
+        }
+      });
+    });
+  });
+}
